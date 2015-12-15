113489d951b87fa1beab26fb31a54499c616c324
Rawemu <przekop.rafal@gmail.com>
2015-12-15 17:01:54 +0100

    Added sticky menu javascript to app.js, added sticky class to css

--- js/app.js
+++ js/app.js
@@ -2,4 +2,16 @@
  * Created by coderslab on 15.12.15.
  */
 
-console.log("working");
+$(document).ready(function(){
+    var menu = $("#menu");
+    var menuOffsetFromTop = menu.position().top;
+
+    $(window).on("scroll", function(){
+        if($(window).scrollTop() > menuOffsetFromTop){
+            menu.addClass("sticky");
+        }
+        else{
+            menu.removeClass("sticky");
+        }
+    });
+});
